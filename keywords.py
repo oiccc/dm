8c3f37b071e104fd3d334f8b0f9d83af975eee75
oiccc <cimer_oic@163.com>
2016-09-16 17:38:49 +0800

    Update keywords.py

--- keywords.py
+++ keywords.py
@@ -2,12 +2,14 @@
 import sys
 sys.path.append('../')
 
+import os
 import jieba
 
 jieba.load_userdict("userdict.txt")
-
 import jieba.analyse
 from optparse import OptionParser
+
+
 
 USAGE = "usage:    python extract_tags_with_weight.py [file name] -k [top k] -w [with weight=1 or 0]"
 
@@ -38,10 +40,19 @@ else:
 
 content = open(file_name, 'rb').read()
 
+
+
 tags = jieba.analyse.extract_tags(content, topK=topK, withWeight=withWeight)
+
 
 if withWeight is True:
     for tag in tags:
-        print("tag: %s\t\t weight: %f" % (tag[0],tag[1]))
+        a = os.popen('grep -o '+tag[0].encode("utf-8")+' test.txt|wc -l')     #加入统计词数功能
+#        print("tag: %s\t\t weight: %f" % (tag[0],tag[1]))
+        c = int(a.read())
+        print("tag: %s\t\t weight: %f\t\t count: %d" % (tag[0],tag[1],c))
+
+        
+    
 else:
     print(",".join(tags))
